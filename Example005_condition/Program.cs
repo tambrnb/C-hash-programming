9186ecb9e5164126c7b7157b03916ea85f297a77
Tamerlan <t.boranbayev@yandex.ru>
2022-10-14 21:52:06 +0600

    Created condition

---
+++ Example005_condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower == "Тамерлан")
+{
+        Console.WriteLine ("Ура, это же Тамерлан!");
+}
+else
+{
+    Console.Write ("Привет, "); 
+    Console.WriteLine (username);
+}
