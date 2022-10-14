079603a36f1d24df0a4920961f48fe728576be92
Tamerlan <t.boranbayev@yandex.ru>
2022-10-14 22:00:16 +0600

    Added weights code

---
+++ Example006_Weights/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 4;
+int c = 7;
+int d = 8;
+int e = 25;
+int max = a;
+
+if(a > max) max = a; 
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e; 
+
+Console.Write ("max = ");
+Console.WriteLine (max);
